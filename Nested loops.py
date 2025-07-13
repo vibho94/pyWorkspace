1acfe4dcad785e5e061895c841373f3c191165b2
vibho94 <vibhor.singhal94@gmail.com>
2025-07-13 12:31:07 +0530

    Commit

--- Nested loops.py
+++ Nested loops.py
@@ -7,8 +7,8 @@
 * * * * *
 """
 
-for i in range (1,6):
-    for j in range (1,6):#1 iteration complete
+for i in range (1,7):
+    for j in range (1,7):#1 iteration complete
         print("*",end=" ")
     print()
 
