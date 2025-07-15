430c0b8b86d8282d07d3b9517eccc57f37bab17b
vibho94 <vibhor.singhal94@gmail.com>
2025-07-15 16:24:15 +0530

    Commit 2

--- Nested loops.py
+++ Nested loops.py
@@ -5,15 +5,15 @@
 * * * * *
 * * * * *
 * * * * *
-"""
 
-for i in range (1,7):
-    for j in range (1,7):#1 iteration complete
+
+for i in range (1,6):
+    for j in range (1,6):#1 iteration complete
         print("*",end=" ")
     print()
 
 
-"""
+
 2.) Print the below pattern:-
 *
 * *
@@ -22,8 +22,90 @@ for i in range (1,7):
 * * * * *
 
 
-for i in range (1,6):
-     print("*")
+r=5 # decide the number of rows
+for i in range(1,r+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
 
+3.) Print the below pattern:-
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+
+
+r=5
+for i in range(r,0,-1): #(5,0,-1)
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+
+4. print the below patter :-
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+
+n=5
+for i in range (1,n+1):
+    for j in range (1,i+1):
+        print(j,end=" ")
+    print()
+
+5.) Print the below pattern :-
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+
+
+from qrcode.util import pattern_position
+
+n=5
+for i in range(n,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
+
+6.) Print the below pattern
+
+        *
+      * * *
+    * * * * *
+   * * * * * *
+ * * * * * * * * *
+
+
+
+n=9
+
+for i in range(1,n+1,2):#(1,9)
+    for k in range(n,i,-2):
+        print(" ",end=" ")
+    for j in range(1,i+1):#(1,2)
+        print( "*",end=" ")
+    print()
+
+
+7.) Print the below pattern :-
+
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
 
 """
+
