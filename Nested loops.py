b2efcefb7a1598b33d20632028e9329ac132eae8
vibho94 <vibhor.singhal94@gmail.com>
2025-07-15 17:00:08 +0530

    Commit 2

--- Nested loops.py
+++ Nested loops.py
@@ -109,3 +109,18 @@ for i in range(1,n+1,2):#(1,9)
 
 """
 
+n=9
+for i in range(1,n+1,2):#(1,9)
+    for j in range(n,i,-2):#(9,1)
+        print(" ",end=" ")
+    for k in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+
+for i in range(n-2,0,-2):#(7,1)
+     for j in range(n-i,0,-2):#(2,1)
+         print(" ",end=" ")
+     for k in range(i):#(7,1)
+        print("*",end=" ")
+     print()
