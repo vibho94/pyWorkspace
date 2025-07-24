f757d72c2e45d6bac2f344a7d4e1b5ee2c9efb8c
vibho94 <vibhor.singhal94@gmail.com>
2025-07-24 09:24:33 +0530

    Commit 2

---
+++ list.py
@@ -0,0 +1,18 @@
+#list can contains a group of elements having different data type while array can have single data type elements
+sample_list=["Vibhor",28,49,"M"]
+print(sample_list)
+
+#indexing of a list
+
+sample_list=["Vibhor",20,"M","Noida"]
+print(sample_list[1])
+#slicing of a list
+
+sample_list=["Vibhor",28,"M","Noida"]
+print(sample_list[1:3:2])
+
+#range in the list
+#range(start,stop,step)
+
+a=list(range(1,100,10))
+print(a)
