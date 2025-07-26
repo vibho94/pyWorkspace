96ff1c6656d90cec0f42f539ca9d21257fe40c3f
vibho94 <vibhor.singhal94@gmail.com>
2025-07-26 13:19:06 +0530

    Commit 2

--- question_list.py
+++ question_list.py
@@ -5,7 +5,7 @@ list=list(my_list)
 print(list)
 for i in my_list:
     print(i)
-"""
+
 # create list as per user input and display its sum
 
 
@@ -26,3 +26,33 @@ def sum():
 s=sum()
 print(s)
 
+
+
+# find out maximum and minimum number in the list
+
+def value():
+    lst=[]
+    user=int(input("enter the number you want="))
+    for i in range(user):
+        add=int(input(f"enter the element {i}="))
+        lst.append(add)
+    print(lst)
+    maximum=max(lst)
+    print(f"The maximum is {maximum}")
+    minimum=min(lst)
+    print(f"The minimum is {minimum}")
+
+value()
+
+"""
+
+#3.) Find the common element from two lists
+
+def common():
+     lst1=[1,2,3,4,5,6]
+     lst2=[2,3,4,1,2,7,8]
+     s1=set(lst1)
+     s2=set(lst2)
+     unique=s1.intersection(s2)
+     print(unique)
+common()
